ea522340eed2e18c85f2ec063f392d07926f4495
ChenZhen <chenzhennba@gmail.com>
2025-04-22 19:53:30 +1000

    Initial project.

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by ChenZhen on 22/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
