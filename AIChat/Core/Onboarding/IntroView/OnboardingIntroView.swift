23c63085175e64c91006e4d467cc4a2e73176cf5
ChenZhen <chenzhennba@gmail.com>
2025-04-23 19:29:24 +1000

    [Onboarding Flow / OnboardingIntroView & OnboardingColorView] [Feature] OnboardingIntroView, OnboardingColorView

---
+++ AIChat/Core/Onboarding/IntroView/OnboardingIntroView.swift
@@ -0,0 +1,52 @@
+//
+//  OnboardingIntroView.swift
+//  AIChat
+//
+//  Created by ChenZhen on 23/4/25.
+//
+
+import SwiftUI
+
+struct OnboardingIntroView: View {
+    var body: some View {
+        VStack {
+            Group {
+                Text("Make your own ")
+                +
+                Text("avatars ")
+                    .foregroundStyle(.accent)
+                    .fontWeight(.semibold)
+                +
+                Text("and chat with them!")
+                +
+                Text("\n\n")
+                +
+                Text("Have ")
+                +
+                Text("real conversations ")
+                    .foregroundStyle(.accent)
+                    .fontWeight(.semibold)
+                +
+                Text("with AI generated responses.")
+            }
+            .baselineOffset(6)
+            .frame(maxHeight: .infinity)
+            .padding(24)
+
+            NavigationLink {
+                OnboardingColorView()
+            } label: {
+                Text("Get Started")
+                    .callToActionButton()
+            }
+        }
+        .padding(24)
+        .font(.title3)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        OnboardingIntroView()
+    }
+}
