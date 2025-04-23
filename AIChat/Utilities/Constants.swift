1a759220af3129925a7c1d4330643fba98f33b6e
ChenZhen <chenzhennba@gmail.com>
2025-04-23 16:52:55 +1000

    [Onboarding Flow / WelcomeView & ImageLoaderView] [Feature] Add WelcomeView UI

---
+++ AIChat/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  AIChat
+//
+//  Created by ChenZhen on 23/4/25.
+//
+import Foundation
+
+struct Constants {
+    static let randomImage = "https://picsum.photos/600/600"
+    static let privacyPolicyUrl = "https://www.apple.com"
+    static let termsOfServiceUrl = "https://www.apple.com"
+}
